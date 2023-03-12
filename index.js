1db701fb09aa41ce4b4fd6831d9f1b00afafc47c
skytool <albert.do@aol.com>
2023-03-11 22:07:37 -0600

    I have to figure it out more about last Qs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {name: "Cindy", address:"101 main street"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]: value,};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+    const employee1 = {...employee};
+    delete employee1[key];
+ 
+    return employee1;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee){
+    delete employee.name;
+    return employee;
+
+}
